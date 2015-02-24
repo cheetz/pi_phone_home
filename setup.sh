c26e28468aad71455a222b2e170398a22b02bb15
Peter Kim <tweaked540@gmail.com>
2015-02-23 23:31:08 -0800

    Update

--- setup.sh
+++ setup.sh
@@ -2,6 +2,7 @@ apt-get install openssh-server
 apt-get install openssh-client
 apt-get install sshpass
 ssh-keygen
-sed 's/Strictmodes\ yes/Strictmodes\ no/g' /etc/ssh/sshd_config > /etc/ssh/sshd_config
+cp /etc/ssh/sshd_config /etc/ssh/sshd_config.orig
+sed 's/StrictModes\ yes/StrictModes\ no/g' /etc/ssh/sshd_config > /etc/ssh/sshd_config
 ssh-copy-id root@127.0.0.1
 crontab /opt/pi_phone_home/cron.txt
