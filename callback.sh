9f1b6a8076509ef83bf6319762fd1e11ec0ddf4b
Peter Kim <tweaked540@gmail.com>
2015-02-23 23:44:12 -0800

    Update

--- callback.sh
+++ callback.sh
@@ -2,5 +2,5 @@
 if ps -ef | grep -v grep | grep yourserver.com ; then
 exit 0
 else
-  sshpass -p 'password' ssh -f -N -T -R2221:localhost:22 yourserver.com -p22 -l username >> /dev/null &
+  sshpass -p 'password' ssh -o "StrictHostKeyChecking no" -f -N -T -R2221:localhost:22 yourserver.com -p22 -l username >> /dev/null &
 fi
